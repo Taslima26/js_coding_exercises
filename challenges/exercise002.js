00c035477b1cfcd5f6f289b6793a26ebc8d30ec2
Taslimabanu banglawala <bangl1@MacBook-Pro.local>
2021-01-27 14:57:33 +0000

    exercise002 completed

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,36 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  let result = person.city === "Manchester" ? true : false;
+  return result;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let sheepArray = arr.filter((animal) => animal === "sheep");
+  return sheepArray.length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return person.address.postCode.startsWith("M") &&
+    person.address.city === "Manchester"
+    ? true
+    : false;
 }
 
 module.exports = {
@@ -28,5 +38,5 @@ module.exports = {
   isFromManchester,
   countSheep,
   getBusNumbers,
-  hasMPostCode
+  hasMPostCode,
 };
