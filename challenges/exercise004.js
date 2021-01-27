ce8c464569ecee52b67928bc09812d9379b2ecc2
Taslimabanu banglawala <bangl1@MacBook-Pro.local>
2021-01-27 20:14:15 +0000

    exercise004 solved

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,32 +1,48 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  let smallNumberArray = nums.filter((num) => num < 1);
+  return smallNumberArray;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   // Your code here
+  let namesBeginWithChar = names.filter((name) => name.charAt(0) === char);
+  return namesBeginWithChar;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
+  let verbArray = words.filter((word) => word.startsWith("to "));
+  return verbArray;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  let numsArray = nums.filter((num) => Number.isInteger(num));
+  return numsArray;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
+  let citiesArray = [];
+  users.forEach((user) => citiesArray.push(user.data.city.displayName));
+  return citiesArray;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  let squaredNumbers = nums.map((num) => Math.sqrt(num));
+  let squaredRoundedNumbers = squaredNumbers.map(
+    (num) => Math.round(num * 100) / 100
+  );
+  return squaredRoundedNumbers;
 }
 
 function findSentencesContaining(sentences, str) {
@@ -48,5 +64,5 @@ module.exports = {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 };
